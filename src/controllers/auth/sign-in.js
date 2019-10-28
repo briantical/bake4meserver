fdcf1ebbffa0fef5e8cdbac346da8ec9c350e109
Lutaaya Brian Ivan <briantical@briantical.local>
2019-10-28 22:48:18 +0300

    verify user before signup

--- src/controllers/auth/sign-in.js
+++ src/controllers/auth/sign-in.js
@@ -2,9 +2,14 @@ const { sendOne } = require('../../middleware');
 const { User } = require('../../models/user/');
 
 const signIn = async (req, res ,next) => {
-  const { token, user } = req; 
-  return sendOne(res, { user, token });
+  try {    
+    let { token, user , user:{verification:{isVerified}}} = req;
+    let message = 'User is not verified';
+    isVerified ?  sendOne(res, { user, token }) : sendOne(res, { message });
 
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = signIn;
