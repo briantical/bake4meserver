45c21d54e2a08544b702b3c8aaf00fbad17cf125
Lutaaya Brian Ivan <briantical@briantical.local>
2019-10-30 20:54:21 +0300

    Working with gcloud and new mongodb atlas instance

--- src/models/category/schema.js
+++ src/models/category/schema.js
@@ -9,13 +9,7 @@ const schema = new Schema({
   description:{
     type : String,    
     required: [true]
-  },
-  priceRange : {
-    type : String,
-    default : "100000 - 3000000",
-    required : [true]
   }
-  //Add a price range for each category
 });
 
 module.exports = { schema };
