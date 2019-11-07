b8c8af87266310b6e28a74764aa6c45bcb2e70f7
Lutaaya Brian Ivan <briantical@briantical.local>
2019-11-08 00:22:15 +0300

    Send email and create user

--- src/controllers/auth/sign-up.js
+++ src/controllers/auth/sign-up.js
@@ -41,21 +41,19 @@ const signUp = ({ User }, { config }) => (req, res, next) => {
         html : `<div>Follow the link below to verify your email.<br><a href='http://localhost:3000/api/v1/auth/verify${_token}'>VERIFY</a></div>`
     };
 
-    User.register(user, password, function name(err,user) {
-      if (err === null) { 
-        
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            res.status(404).json({ error });
-            return;
-          }else{
-            console.log('Message sent: %s', info.messageId);
-            return withoutErrors(next, () => next());
-          }
-        });
-       }
-       //return sendOne(res,  err );
-    })
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error)
+        res.status(404).json({ error });
+        return;
+      }else{
+        User.register(user, password, function name(err,user) {
+          console.log('Message sent: %s', info.messageId);
+          return withoutErrors(next, () => next());
+        })
+      }
+    });
 });
 
 };
