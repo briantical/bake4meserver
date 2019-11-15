611246ad07b0d20d3a66d0904b6663bb2e9a85f7
Lutaaya Brian Ivan <briantical@briantical.local>
2019-11-15 18:48:36 +0300

    Working with Heroku

--- src/controllers/auth/sign-up.js
+++ src/controllers/auth/sign-up.js
@@ -1,58 +1,64 @@
-const { withoutErrors, sendOne } = require('../../middleware');
-const { NotAcceptable } = require('rest-api-errors');
-const { PASSWORD } = require('../../utils/regexes');
+const { withoutErrors, sendOne } = require("../../middleware");
+const { NotAcceptable } = require("rest-api-errors");
+const { PASSWORD } = require("../../utils/regexes");
 
-const nodemailer = require('nodemailer');
-const _ = require('lodash');
+const nodemailer = require("nodemailer");
+const _ = require("lodash");
 
 const signUp = ({ User }, { config }) => (req, res, next) => {
-  const { email, password} = req.body;
+  const { email, password } = req.body;
 
   if (!PASSWORD.test(password)) {
-    return next(new NotAcceptable(406, 'Password is in wrong format.'));
+    return next(new NotAcceptable(406, "Password is in wrong format."));
   }
 
   nodemailer.createTestAccount((err, account) => {
     let transporter = nodemailer.createTransport({
-        host: 'smtp.googlemail.com', // Gmail Host
-        port: 465, // Port
-        secure: true, // this is true as port is 465
-        auth: {
-            user: config.gmail_user, //Gmail username
-            pass: config.gmail_pass // Gmail password
-        }
+      host: "smtp.googlemail.com", // Gmail Host
+      port: 465, // Port
+      secure: true, // this is true as port is 465
+      auth: {
+        user: config.gmail_user, //Gmail username
+        pass: config.gmail_pass // Gmail password
+      }
     });
 
-    let _token = Math.random().toString(36).substring(7) + new Date().getTime();
+    let _token =
+      Math.random()
+        .toString(36)
+        .substring(7) + new Date().getTime();
     const user = new User({
       email: email
     });
-    
-    _.extend(user,{
-      verification:{
+
+    _.extend(user, {
+      verification: {
         _token
       }
     });
 
     let mailOptions = {
-        from: '"CRITERIA CAKES" <admin@criteriacakes.ug>',
-        to: email, 
-        subject: 'Confirm Email',
-        html : `<div>Follow the link below to verify your email.<br><a href='http://localhost:3000/api/v1/auth/verify${_token}'>VERIFY</a></div>`
+      from: '"CRITERIA CAKES" <admin@criteriacakes.ug>',
+      to: email,
+      subject: "Confirm Email",
+      html: `<div>Follow the link below to verify your email.<br><a href='http://localhost:3000/api/v1/auth/verify${_token}'>VERIFY</a></div>`
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
-        console.log(error)
-         res.status(404).json({ error });
-         return;
-      }else{
-         console.log('Message sent: %s', info.messageId);
-         User.register(user, password, withoutErrors(next, () => next()))
-       }
+        console.log(error);
+        res.status(404).json({ error });
+        return;
+      } else {
+        console.log("Message sent: %s", info.messageId);
+        User.register(
+          user,
+          password,
+          withoutErrors(next, () => next())
+        );
+      }
     });
-});
-
+  });
 };
 
 module.exports = signUp;
--- src/mongo/MongoManager.js
+++ src/mongo/MongoManager.js
@@ -1,31 +1,30 @@
-const mongoose = require('mongoose');
-
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+const mongoose = require("mongoose");
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
 class MongoManager {
-  constructor () {
-  }
+  constructor() {}
 
-  getMongoUrl() {  	
+  getMongoUrl() {
     return process.env.MONGODB_URI;
   }
-  
-  connect () {
-    mongoose.connect(this.getMongoUrl(), { 
-      useUnifiedTopology : true,
-    	useNewUrlParser: true ,
-    	keepAlive: true, 
-      keepAliveInitialDelay: 300000 
-    })
-      .then(() => {
-        console.log('Connection to database established.');
 
-        return mongoose.connection.db
+  connect() {
+    mongoose
+      .connect(this.getMongoUrl(), {
+        useNewUrlParser: true,
+        keepAlive: true,
+        keepAliveInitialDelay: 300000
+      })
+      .then(() => {
+        console.log("Connection to database established.");
+
+        return mongoose.connection.db;
       })
       .catch(err => {
         setTimeout(() => {
-          console.log('Attempting to connect ...');
+          console.log("Attempting to connect ...");
           this.connect();
         }, 2000);
       });
