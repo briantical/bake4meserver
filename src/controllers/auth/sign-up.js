bec4d0f5eb5b3d7b4e9898ca55c61dcb5be44d25
briantical <lutbrianivan@gmail.com>
2020-03-03 11:04:12 +0300

    Changed the sent email domain name link

--- src/controllers/auth/sign-up.js
+++ src/controllers/auth/sign-up.js
@@ -41,7 +41,7 @@ const signUp = ({ User }, { config }) => (req, res, next) => {
       from: '"BAKE4ME" <admin@criteriacakes.ug>',
       to: email,
       subject: "Confirm Email",
-      html: `<div>Follow the link below to verify your email.<br><a href='http://localhost:3000/api/v1/auth/verify${_token}'>VERIFY</a></div>`
+      html: `<div>Follow the link below to verify your email.<br><a href='${process.env.DOMAIN_NAME}/api/v1/auth/verify${_token}'>VERIFY</a></div>`
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
