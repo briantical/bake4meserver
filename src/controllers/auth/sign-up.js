94320ab07f5a33824dd2aca7b0cd138af69e636a
briantical <lutbrianivan@gmail.com>
2020-03-04 12:04:32 +0300

    Reconfigured the the nodemailer

--- src/controllers/auth/sign-up.js
+++ src/controllers/auth/sign-up.js
@@ -3,7 +3,21 @@ const { NotAcceptable } = require("rest-api-errors");
 const { PASSWORD } = require("../../utils/regexes");
 
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 const _ = require("lodash");
+
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.GOOGLE_CLIENT_ID, //Client ID
+  process.env.GOOGLE_CLIENT_SECRET, //Client secret
+  "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+});
+const accessToken = oauth2Client.getAccessToken();
 
 const signUp = ({ User }, { config }) => (req, res, next) => {
   const { email, password } = req.body;
@@ -12,52 +26,53 @@ const signUp = ({ User }, { config }) => (req, res, next) => {
     return next(new NotAcceptable(406, "Password is in wrong format."));
   }
 
-  nodemailer.createTestAccount((err, account) => {
-    let transporter = nodemailer.createTransport({
-      host: "smtp.googlemail.com", // Gmail Host
-      port: 465, // Port
-      secure: true, // this is true as port is 465
-      auth: {
-        user: config.gmail_user, //Gmail username
-        pass: config.gmail_pass // Gmail password
-      }
-    });
+  const smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: config.gmail_user,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      accessToken: accessToken
+    }
+  });
 
-    let _token =
-      Math.random()
-        .toString(36)
-        .substring(7) + new Date().getTime();
-    const user = new User({
-      email: email
-    });
+  let _token =
+    Math.random()
+      .toString(36)
+      .substring(7) + new Date().getTime();
+  const user = new User({
+    email: email
+  });
 
-    _.extend(user, {
-      verification: {
-        _token
-      }
-    });
+  _.extend(user, {
+    verification: {
+      _token
+    }
+  });
 
-    let mailOptions = {
-      from: '"BAKE4ME" <admin@criteriacakes.ug>',
-      to: email,
-      subject: "Confirm Email",
-      html: `<div>Follow the link below to verify your email.<br><a href='${process.env.DOMAIN_NAME}/api/v1/auth/verify${_token}'>VERIFY</a></div>`
-    };
+  let mailOptions = {
+    from: '"BAKE4ME" <noreply@bake4me.ug>',
+    to: email,
+    subject: "Email Verification",
+    html: `<div>Follow the link below to verify your email.<br><a href='${process.env.DOMAIN_NAME}/api/v1/auth/verify${_token}'>VERIFY</a></div>`
+  };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        res.status(404).json({ error });
-        return;
-      } else {
-        console.log("Message sent: %s", info.messageId);
-        User.register(
-          user,
-          password,
-          withoutErrors(next, () => next())
-        );
-      }
-    });
+  smtpTransport.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(404).json({ error });
+      smtpTransport.close();
+      return;
+    } else {
+      console.log("Message sent: %s", info.messageId);
+      User.register(
+        user,
+        password,
+        withoutErrors(next, () => next())
+      );
+    }
   });
 };
 
