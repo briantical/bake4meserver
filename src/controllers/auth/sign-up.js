689f6cfc382a543911597bae47cbb1d9a0067a50
briantical <lutbrianivan@gmail.com>
2020-03-06 13:13:49 +0300

    Working emails with Auth2

--- src/controllers/auth/sign-up.js
+++ src/controllers/auth/sign-up.js
@@ -6,18 +6,18 @@ const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const _ = require("lodash");
 
-// const OAuth2 = google.auth.OAuth2;
+const OAuth2 = google.auth.OAuth2;
 
-// const oauth2Client = new OAuth2(
-//   process.env.GOOGLE_CLIENT_ID, //Client ID
-//   process.env.GOOGLE_CLIENT_SECRET, //Client secret
-//   "https://developers.google.com/oauthplayground" // Redirect URL
-// );
+const oauth2Client = new OAuth2(
+  process.env.GOOGLE_CLIENT_ID, //Client ID
+  process.env.GOOGLE_CLIENT_SECRET, //Client secret
+  "https://developers.google.com/oauthplayground" // Redirect URL
+);
 
-// oauth2Client.setCredentials({
-//   refresh_token: process.env.GOOGLE_REFRESH_TOKEN
-// });
-// const accessToken = oauth2Client.getAccessToken();
+oauth2Client.setCredentials({
+  refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+});
+const accessToken = oauth2Client.getAccessToken();
 
 const signUp = ({ User }, { config }) => (req, res, next) => {
   const { email, password } = req.body;
@@ -26,24 +26,15 @@ const signUp = ({ User }, { config }) => (req, res, next) => {
     return next(new NotAcceptable(406, "Password is in wrong format."));
   }
 
-  // const smtpTransport = nodemailer.createTransport({
-  //   service: "gmail",
-  //   auth: {
-  //     type: "OAuth2",
-  //     user: config.gmail_user,
-  //     clientId: process.env.GOOGLE_CLIENT_ID,
-  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  //     refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-  //     accessToken: accessToken
-  //   }
-  // });
-
-  var smtpTransport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
+  const smtpTransport = nodemailer.createTransport({
+    service: "gmail",
     auth: {
-      user: "21f13fe58f3c78",
-      pass: "6b3b47378a6830"
+      type: "OAuth2",
+      user: config.gmail_user,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      accessToken: accessToken
     }
   });
 
@@ -65,6 +56,7 @@ const signUp = ({ User }, { config }) => (req, res, next) => {
     from: '"BAKE4ME" <noreply@bake4me.ug>',
     to: email,
     subject: "Email Verification",
+    generateTextFromHTML: true,
     html: `<div>Follow the link below to verify your email.<br><a href='${process.env.DOMAIN_NAME}/api/v1/auth/verify${_token}'>VERIFY</a></div>`
   };
 
